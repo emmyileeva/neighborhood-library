2a1efc52b5a85faa1cff7184156c8e47d63ab311
Emiliya Ileeva <emilyileeva@gmail.com>
2025-04-21 14:10:55 -0700

    Create array wit 20 books and iterate through in Main class

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -2,5 +2,12 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
+        // Create array to hold 20 books
+        Book[] books = new Book[20];
+
+        // Iterate through the array and create Book objects
+        for (int i = 0; i < books.length; i++) {
+            books[i] = new Book(i + 1, "ISBN" + (i + 1), "Title" + (i + 1));
+        }
     }
 }
